660f69376d6bbde6622138a64003a7372c977420
bptiago <125921557+bptiago@users.noreply.github.com>
2024-03-06 14:31:52 -0300

    Create Users route

--- server/index.js
+++ server/index.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const app = express();
 
-// Cors
+// JSON bodies
+app.use(express.json());
+
 const cors = require("cors");
 app.use(cors());
 
-// Router
 const usersRouter = require("./routes/Users");
 app.use("/users", usersRouter);
 const todoItemsRouter = require("./routes/TodoItems");
 app.use("/items", todoItemsRouter);
 
-// Init db and server
 const db = require("./models");
 db.sequelize.sync().then(() => {
   app.listen(8080, () => {
